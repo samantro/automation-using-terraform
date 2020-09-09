1631941ad62c0ee064b9d47bc7e04eeb52385174
samantro <66985251+samantro@users.noreply.github.com>
2020-09-10 00:35:27 +0530

    Create wordpress.tf

---
+++ wordpress.tf
@@ -0,0 +1,59 @@
+resource "null_resource" "minikubestart" {
+ provisioner "local-exec" {
+      command = "minikube start"
+   }
+}
+provider "kubernetes" {
+ config_context_cluster = "minikube"
+}
+resource "kubernetes_deployment" "wordpress" {
+ metadata {
+  name = "wp"
+ }
+spec {
+ replicas = 3
+ selector {
+  match_labels = {
+   env = "production"
+   region = "IN"
+   App = "wordpress"
+  }
+  match_expressions {
+   key = "env"
+   operator = "In"
+   values = ["production" , "webserver"]
+  }
+ }
+ template {
+  metadata {
+   labels = {
+    env = "production"
+    region = "IN"
+    App = "wordpress"
+   }
+  }
+  spec {
+   container {
+    image = "wordpress:4.8-apache"
+    name = "wp" 
+    }
+   }
+  }
+ }
+}
+resource "kubernetes_service" "wordpresslb" {
+ metadata {
+  name = "wplb"
+ }
+ spec {
+  selector = {
+   app = "wordpress"
+  }
+  port {
+   protocol = "TCP"
+   port = 80
+   target_port = 80
+  }
+  type = "NodePort"
+ }
+}
